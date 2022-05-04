eb8da29ffe106888f491c928227c9ab9524230a6
DaniAvolio <avoliodaniele9@gmail.com>
2022-05-04 17:12:13 +0200

    feat : add Class ClientHandler that manage the accepting of the client and the process

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -11,16 +11,18 @@ import java.net.Socket;
  * SERVER!!
  * CREATE A SIMPLE SERVER THAT RESPONSE WITH THE SUM OF TWO PARAMETERS
  */
-public class App
-{
+public class App {
 
-
+    static Socket clientSocket;
     static int portNumber = 1234;
-    public static void main( String[] args )
-    {
-            //inizialize
-            System.out.println("Server Started...");
-            ServerSocket serverSocket = null;
+
+    public static void main(String[] args) {
+        ClientHandler ch = new ClientHandler();
+        //inizialize
+        System.out.println("Server Started...");
+        ServerSocket serverSocket = null;
+        while (true) {
+
 
             //inizialize serversocket
             try {
@@ -30,7 +32,7 @@ public class App
             }
 
             //inizialize clientsocket and inizialize conncetion!
-            Socket clientSocket = null;
+
             System.out.println("Accepting...");
             try {
                 assert serverSocket != null;
@@ -39,60 +41,17 @@ public class App
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
-            //receive bufferedreader for input
-            BufferedReader in = null;
-            try {
-                assert clientSocket != null;
-                in = new BufferedReader(
-                        new InputStreamReader(clientSocket.getInputStream()));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-
-            //inizialiaze printwriter for output, so for the response of the server
-            PrintWriter out = null;
-            try {
-                out = new PrintWriter(clientSocket.getOutputStream(), true);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
+            ch.ManageClient();
             //call of the function for the process
-            assert in != null;
-            processl(in, out);
-
-
-    }
-    //funcion process : server receive input from the cleint, process and responde in ths functions
-        private static void processl(BufferedReader in, PrintWriter out){
-    try {
-        String s;
-        while ((s = in.readLine()) != null)  {
-            System.out.println("Received : "+s);
-
-            if(s.contains("+")){
-                int iz=s.indexOf("+");
-               int c1 = Integer.parseInt( s.substring(0,iz) );
-               int c2 = Integer.parseInt( s.substring(iz+1) );
-               int somma= c1+c2;
-               out.println("Risultato: "+somma);
-                out.flush();
+            ch.processl();
+            try {
+                clientSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            else{
-                out.println("you have not entered any operations");
-                out.flush();
-            }
-            //out.println(s.toUpperCase());
-
         }
-    } catch (IOException e) {
-        e.printStackTrace();
+
+
     }
 }
-
-
-
-    }
 
